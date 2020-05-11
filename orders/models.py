d9433e94e5739d86ba2651f651d096d6f548c0ca
syadallah <syadallah@gmail.com>
2020-05-11 17:38:22 -0400

    Add Admin and Model for MenuItem

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+  group = models.CharField(max_length=64)
+  item = models.CharField(max_length=64)
+    return f"{self.group}, {self.item} | small: ${self.price_sm} | large: ${self.price_lg} | single size: ${self.price}"
