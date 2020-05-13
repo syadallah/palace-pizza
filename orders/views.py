d86324a8985059f3f4a94a36bae97ff39dc61116
syadallah <syadallah@gmail.com>
2020-05-13 16:54:22 -0400

    Render login to views.py

--- orders/views.py
+++ orders/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "orders/index.html")
+def login_view(request):
+        return render(request, "orders/login.html")
