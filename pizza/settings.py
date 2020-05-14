00b41d579a8f56c84b159bc020ad227425c5d605
syadallah <syadallah@gmail.com>
2020-05-14 17:47:09 -0400

    Deploy to heroku

--- pizza/settings.py
+++ pizza/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Add python-dotenv to manage local environment variables
+from dotenv import load_dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -126,3 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Activate Django-Heroku
 import django_heroku
 django_heroku.settings(locals()) # this line has to occur after STATIC_ROOT
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
