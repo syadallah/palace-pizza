5eef03273e1c21b26e3e311ac707e424c8871617
syadallah <syadallah@gmail.com>
2020-05-15 16:59:52 -0400

    Render MenuItem
    Use Admin to insert data (menu and prices according) to Pinocchios-Pizza
    Using Jinja2 to render the menu on login.html with table tags
    Add context login_view function in the view file
    "menu": MenuItem.objects.all()
    Import MenuItem from orders.model

--- orders/views.py
+++ orders/views.py
@@ -7,28 +7,7 @@ from django.core import serializers
 def index(request):
     return render(request, "orders/index.html")
 def login_view(request):
-  # POST
-  if request.method == 'POST':
 
-    # Grab username & password submitted via POST request & make sure that both
-    # fields are not empty
-    username = request.POST["username"]
-    password = request.POST["password"]
-    if username == '' or password == '':
-      return HttpResponse('{"success": false, "message": "Both username and password are required."}')
-
-    # Django built-in username & password authentication + login session -- by
-    # logging the user in, request.user.is_authenticated == True in the
-    # def index(request): route.
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-      login(request, user)
-      return HttpResponse('{"success": true, "message": ""}')
-    else:
-      return HttpResponse('{"success": false, "message": "Invalid username and/or password."}')
-
-  # GET
-  else:
     context = {
       "menu": MenuItem.objects.all(),
     }
