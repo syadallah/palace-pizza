a526a4ba923aa632c2d2ecef6a9e1a89137fbb70
syadallah <syadallah@gmail.com>
2020-05-16 16:31:03 -0400

    Import Topping to view.py model

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from orders.models import MenuItem
+from orders.models import MenuItem, Topping
 from django.core import serializers
 from django.contrib.auth.models import User
 
