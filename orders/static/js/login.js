e6d63f992f45321f5610c1c43afce2bbfaaf503c
syadallah <syadallah@gmail.com>
2020-05-16 15:17:31 -0400

    Save all entered registration information

--- orders/static/js/login.js
+++ orders/static/js/login.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // ==================== REGISTER ==============================================
 
+    document.querySelector('#submit-register').onclick = () => {
+      // Ensure that all registration information was entered
+    const username = document.querySelector('#register-username').value;
+    const password = document.querySelector('#register-password').value;
+    const first_name = document.querySelector('#register-first-name').value;
+    const last_name = document.querySelector('#register-last-name').value;
+    const email = document.querySelector('#register-email').value;
+ };
 });
