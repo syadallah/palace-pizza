d6373f7f4ef6566f763c180d729cf7d70b66b6f6
syadallah <syadallah@gmail.com>
2020-05-16 16:36:08 -0400

    Add OrderHistory model
    $ python3 manage.py makemigrations
    $ python3 manage.py migrate

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import MenuItem, Topping, Extra
+from .models import MenuItem, Topping, Extra, OrderHistory
 # Register your models here.
 
 admin.site.register(MenuItem)
 admin.site.register(Topping)
 admin.site.register(Extra)
+admin.site.register(OrderHistory)
--- orders/models.py
+++ orders/models.py
@@ -14,10 +14,23 @@ class Topping(models.Model):
   item      = models.CharField(max_length=64)
   def __str__(self):
     return f"{self.item}"
-    
+
 class Extra(models.Model):
   item      = models.CharField(max_length=64)
   price_sm  = models.DecimalField(max_digits=5, decimal_places=2)
   price_lg  = models.DecimalField(max_digits=5, decimal_places=2)
   def __str__(self):
     return f"{self.item}"
+    
+class OrderHistory(models.Model):
+  created       = models.DateTimeField(auto_now_add=True)
+  data_group    = models.CharField(max_length=64, default=None)
+  data_size     = models.CharField(max_length=64, default=None)
+  extras        = models.CharField(max_length=64, default=None)
+  extras_price  = models.DecimalField(max_digits=5, decimal_places=2, default=None)
+  name          = models.CharField(max_length=64, default=None)
+  price         = models.DecimalField(max_digits=5, decimal_places=2, default=None)
+  toppings      = models.CharField(max_length=64, default=None)
+  user          = models.CharField(max_length=64, default=None)
+  def __str__(self):
+    return f"created: {self.created} | data_group: {self.data_group} | data_size: {self.data_size} | extras: {self.extras} | extras_price: {self.extras_price} | name: {self.name} | price: {self.price} | toppings: {self.toppings} | user: {self.user}"
