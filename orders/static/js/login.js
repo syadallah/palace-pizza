d8723eeaa1707c81ecb18b35a0c3ae586be70ce3
syadallah <syadallah@gmail.com>
2020-05-16 15:25:06 -0400

    Initialize POST request, extract the CSRF value from the index.html DOM

--- orders/static/js/login.js
+++ orders/static/js/login.js
@@ -8,5 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const first_name = document.querySelector('#register-first-name').value;
     const last_name = document.querySelector('#register-last-name').value;
     const email = document.querySelector('#register-email').value;
+    if (username !== '' && password !== '' && first_name !== '' && last_name !== '' && email !== '') {
+
+    // Initialize POST request, extract the CSRF value from the index.html DOM,
+    // and put that into the header of the POST request.
+    const request = new XMLHttpRequest();
+    request.open('POST', '/register');
+    const csrf_token = document.querySelector('#csrf').childNodes[0]['value'];
+    request.setRequestHeader("X-CSRFToken", csrf_token);
  };
 });
