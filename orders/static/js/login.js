b0597caaa570245c1b4931f3c448070207d62ce4
syadallah <syadallah@gmail.com>
2020-05-16 16:18:36 -0400

    Initialize POST request to login
    FormData() object used to transmit data to the server
    Send login_data (aka FormData()) to views.py
    If views.py returns a JSON response where success = true, then
    the page is redirected to index.html

--- orders/static/js/login.js
+++ orders/static/js/login.js
@@ -51,4 +51,51 @@ document.addEventListener('DOMContentLoaded', function() {
    } else {
      document.querySelector('.error-register').innerHTML = 'All fields are required.'
    };
+   // ==================== LOGIN ==================================================
+
+ document.querySelector('#submit-login').onclick = () => {
+
+   // Ensure that both a username and password were entered
+   const username = document.querySelector('#username').value;
+   const password = document.querySelector('#password').value;
+   if (username !== '' && password !== '') {
+
+     // Initialize POST request, extract the CSRF value from the index.html DOM,
+     // and put that into the header of the POST request.
+     const request = new XMLHttpRequest();
+     request.open('POST', '/login');
+     const csrf_token = document.querySelector('#csrf').childNodes[0]['value'];
+     request.setRequestHeader("X-CSRFToken", csrf_token);
+
+     // The FormData() object can be used to transmit data to the server (ie.
+     // transmit data to views.py).
+     const login_data = new FormData();
+     login_data.append('username', username);
+     login_data.append('password', password);
+
+     // Send login_data (aka FormData()) to views.py, followed by a callback
+     // function that executes once a response is received from views.py.
+     request.send(login_data);
+     request.onload = () => {
+       const response = request.responseText;
+       const success = JSON.parse(response)['success'];
+       const error_message = JSON.parse(response)['message'];
+
+       // If views.py returns a JSON response where success = true, then
+       // the page is redirected to index.html.
+       if (success === true) {
+         window.location.href = '/';
+       } else {
+         document.querySelector('.error-login').innerHTML = error_message;
+       }
+     };
+
+     // Disable the default page reload after a POST request.
+     return false;
+
+   // Return an error message if any of the fields were left blank.
+   } else {
+     document.querySelector('.error-login').innerHTML = 'Both username and password are required.'
+   };
+ };
 });
--- orders/views.py
+++ orders/views.py
@@ -7,14 +7,37 @@ from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     return render(request, "orders/index.html")
+    
 def login_view(request):
+  # POST
+  if request.method == 'POST':
 
+    # Grab username & password submitted via POST request & make sure that both
+    # fields are not empty
+    username = request.POST["username"]
+    password = request.POST["password"]
+    if username == '' or password == '':
+      return HttpResponse('{"success": false, "message": "Both username and password are required."}')
+
+    # Django built-in username & password authentication + login session -- by
+    # logging the user in, request.user.is_authenticated == True in the
+    # def index(request): route.
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return HttpResponse('{"success": true, "message": ""}')
+    else:
+      return HttpResponse('{"success": false, "message": "Invalid username and/or password."}')
+
+  # GET
+  else:
     context = {
       "menu": MenuItem.objects.all(),
+      "toppings": serializers.serialize('json', Topping.objects.all()),
+      "extras": serializers.serialize('json', Extra.objects.all()),
     }
     # print('===== LOGIN ===== context: ', context)
     return render(request, "orders/login.html", context)
-
 # ============================ REGISTER ==========================================
 
 def register_view(request):
@@ -40,7 +63,7 @@ def register_view(request):
     user.first_name = first_name
     user.last_name = last_name
     user.save()
-    
+
       # Django built-in username & password authentication + login session -- by
     # logging the user in, request.user.is_authenticated == True in the
     # def index(request): route.
