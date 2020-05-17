16e8df3e813e4af7287ead2eb7e6a0132117a861
syadallah <syadallah@gmail.com>
2020-05-17 16:22:53 -0400

    Create list to keep track of active menu item
    Keep track of total price of current selections
    Count the number of items that are associated with the currently logged
    in user

--- orders/static/js/index.js
+++ orders/static/js/index.js
@@ -13,4 +13,25 @@ for (let i = 0; i < inputs.length; i++) {
     select_item(this);
   });
 };
+// Create list to keep track of active menu item selections, initialize total
+// price, initialize the total number of items that have been ordered (ie. the
+// number of items in the shopping cart), go through localStorage and actually
+// count the number of items that are associated with the currently logged in
+// user (which is obtained from the DOM) & add that number to the DOM on intial
+// page load, set the total price of current selections (which should be 0) to
+// $0 on initial page load
+active_selections = []
+let total_price = 0;
+let items_ordered_count = 0;
+let user = document.querySelector('#user').innerHTML;
+
+for (let j = 0; j < localStorage.length; j++) {
+  if (JSON.parse(localStorage.getItem(j)) !== null &&
+      JSON.parse(localStorage.getItem(j))['user'] === user) {
+    items_ordered_count++;
+  };
+};
+
+document.querySelector('#total_price').append(total_price);
+document.querySelector('#number-of-items-ordered').innerHTML = items_ordered_count;
 }
